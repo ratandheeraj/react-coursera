981f4c8e48395ba41a183833929dd547cb926873
ratandheeraj <ratandheeraj@outlook.com>
2020-07-01 23:37:44 +0530

    added controlled form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -33,9 +33,9 @@ class Header extends Component {
         <Navbar dark expand="md">
           <div className="container">
             <NavbarToggler onClick={this.toggleNav} />
-            <NavbarBrand className="mr-auto" href="/">
+            <NavbarBrand className="mr-auto" href="#">
               <img
-                src="assets/images/logo.png"
+                src={"/assets/images/logo.png"}
                 height="30"
                 width="41"
                 alt="Ristorante Con Fusion"
