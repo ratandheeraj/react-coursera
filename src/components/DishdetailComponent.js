00d5754e66764020ec5b91403919faf03093a96a
ratandheeraj <ratandheeraj@outlook.com>
2020-07-02 11:23:42 +0530

    assignment 4, comment form

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,8 +7,108 @@ import {
   CardBody,
   Breadcrumb,
   BreadcrumbItem,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Button,
+  Label,
 } from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
+  handleSubmit(values) {
+    console.log("Current State is: " + JSON.stringify(values));
+    alert("Current State is: " + JSON.stringify(values));
+  }
+  render() {
+    const required = (val) => val && val.length;
+    const maxLength = (len) => (val) => !val || val.length <= len;
+    const minLength = (len) => (val) => val && val.length >= len;
+    return (
+      <div>
+        <Button outline color="secondary" onClick={this.toggleModal}>
+          <span className="fa fa-pencil fa-lg"></span> Submit Comment
+        </Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <Label htmlFor="rating">Rating</Label>
+              <Control.select
+                model=".rating"
+                name="rating"
+                className="form-control"
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Control.select>
+
+              <Label htmlFor="author" className="mt-2">
+                Your Name
+              </Label>
+              <Control.text
+                model=".author"
+                name="author"
+                className="form-control"
+                validators={{
+                  required,
+                  minLength: minLength(3),
+                  maxLength: maxLength(15),
+                }}
+              />
+              <Errors
+                className="text-danger"
+                model=".author"
+                show="touched"
+                messages={{
+                  required: "Required ",
+                  minLength: "Your name must atleast be 3 characters",
+                  maxLength: "Your name cannot exceed 15 characters",
+                }}
+              />
+              <Label htmlFor="comment" className="mt-2">
+                Comments
+              </Label>
+              <Control.textarea
+                model=".comment"
+                name="comment"
+                className="form-control"
+                rows="6"
+              ></Control.textarea>
+              <Button
+                type="submit"
+                className="mt-2"
+                color="primary"
+                onClick={this.toggleModal}
+              >
+                Sumbit
+              </Button>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
+}
 
 class DishDetail extends Component {
   renderDish(dish) {
@@ -77,6 +177,7 @@ class DishDetail extends Component {
           <div className="col-12 col-md-5 m-1">{this.renderDish(dish)}</div>
           <div className="col-md-5 col-12 m-1">
             {this.renderComments(this.props.comments)}
+            <CommentForm />
           </div>
         </div>
       </div>
@@ -84,7 +185,3 @@ class DishDetail extends Component {
   }
 }
 export default DishDetail;
-
-// <div className="col-12 col-md-5 m-1">
-//   <RenderComments comments={props.comments} />
-// </div>
